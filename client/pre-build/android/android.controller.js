7d4ca2ca7dadaf8593451ddd567010a230b30f41
Ash Ryan <ash@ashryanbeats.com>
2015-07-18 16:36:36 -0400

    Added tutorial factory.

--- client/pre-build/android/android.controller.js
+++ client/pre-build/android/android.controller.js
@@ -1,5 +1,6 @@
-app.controller('AndroidController', function($scope, $http) {
+app.controller('AndroidController', function($scope, TutorialFactory) {
   
   $scope.msgFromScope = "...And I'm a message from the HomeController scope, just so you know that I work!";
 
+  $scope.tutorial = TutorialFactory.getOneTutorial();
 });
---
+++ client/pre-build/android/android.factory.js
@@ -0,0 +1,7 @@
+app.factory('TutorialFactory', function($http) {
+  return {
+    getOneTutorial: function() {
+      console.log("Fact!");
+    }
+  };
+});
