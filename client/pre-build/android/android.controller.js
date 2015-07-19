ba3f590e39c1844956c6bc6539b0c73f27041b13
Ash Ryan <ash@ashryanbeats.com>
2015-07-19 16:10:35 -0400

    Commented out some notes.

--- client/pre-build/android/android.controller.js
+++ client/pre-build/android/android.controller.js
@@ -2,9 +2,11 @@ app.controller('AndroidController', function($scope, TutorialFactory, $sce) {
   
   $scope.msgFromScope = "...And I'm a message from the HomeController scope, just so you know that I work!";
 
-  TutorialFactory.getOneTutorial()
-    .then(function(tutorials) {
-      $scope.tutorial = tutorials.data;
-      $scope.title = $sce.trustAsHtml($scope.tutorial[1].title);
-    });
+  // TutorialFactory.getOneTutorial() // this method on the factory currently gets all, and is here as a placeholder.
+  //   .then(function(tutorials) {
+      
+      // Examples
+      // $scope.tutorial = tutorials.data;
+      // $scope.title = $sce.trustAsHtml($scope.tutorial[1].title);
+  //   });
 });
