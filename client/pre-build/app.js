4f0add6e7cc56241497bbcd743eaa4dd3586ce6d
Ash Ryan <ash@ashryanbeats.com>
2015-07-27 09:05:30 -0400

    Styling changes.

--- client/pre-build/app.js
+++ client/pre-build/app.js
@@ -5,4 +5,5 @@ app.config(function ($urlRouterProvider, $locationProvider) {
    $locationProvider.html5Mode(true);
    // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
    $urlRouterProvider.otherwise('/');
+
 });
